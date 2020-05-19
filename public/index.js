4150cd0ff3533e74acd301ca2d4d01639660e9de
Khurram <khurramshahzad@tfl.gov.uk>
2020-05-19 16:25:51 +0100

    add more example
    - Add a simple counter
    - Add a simple timer
    - Fetch with setInterval

--- public/index.js
+++ public/index.js
@@ -16,6 +16,34 @@ notify(reactiveModel, ({ newValue }) => {
   `
 })
 
-fetch("https://randomuser.me/api/")
-  .then((data) => data.json())
-  .then((json) => (reactiveModel.me = json))
+setInterval(() =>fetch("https://randomuser.me/api/")
+.then((data) => data.json())
+.then((json) => (reactiveModel.me = json)) , 5000)
+
+// Timer example
+const time = document.getElementById("time")
+const timeModel = { current: new Date().toString() }
+const reactiveTimeModelBuilder = ReactiveModel(timeModel)
+const reactiveTimeModel = reactiveTimeModelBuilder.buildReactiveModel()
+
+reactiveTimeModelBuilder.notify(reactiveTimeModel, ({ newValue }) => {
+  time.innerHTML = newValue
+})
+setInterval(() => {
+  reactiveTimeModel.current = new Date().toString()
+}, 100)
+
+// SImple counter example
+const counterBtn = document.getElementById("counter-btn")
+const counterValue = document.getElementById("counter-value")
+const counterModel = { value: 0 }
+const reactiveCounterModelBuilder = ReactiveModel(counterModel)
+const reactiveCounterModel = reactiveCounterModelBuilder.buildReactiveModel()
+
+reactiveCounterModelBuilder.notify(reactiveCounterModel, ({ newValue }) => {
+  counterValue.innerHTML = newValue
+})
+
+counterBtn.addEventListener("click", () => {
+  reactiveCounterModel.value++
+})
