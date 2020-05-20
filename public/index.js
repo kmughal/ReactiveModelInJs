918e4ffb0a8498ec76a861af268aa043453a1b27
Khurram <khurramshahzad@tfl.gov.uk>
2020-05-20 10:43:43 +0100

    add portal

--- public/index.js
+++ public/index.js
@@ -16,9 +16,13 @@ notify(reactiveModel, ({ newValue }) => {
   `
 })
 
-setInterval(() =>fetch("https://randomuser.me/api/")
-.then((data) => data.json())
-.then((json) => (reactiveModel.me = json)) , 5000)
+setInterval(
+  () =>
+    fetch("https://randomuser.me/api/")
+      .then((data) => data.json())
+      .then((json) => (reactiveModel.me = json)),
+  5000
+)
 
 // Timer example
 const time = document.getElementById("time")
@@ -47,3 +51,46 @@ reactiveCounterModelBuilder.notify(reactiveCounterModel, ({ newValue }) => {
 counterBtn.addEventListener("click", () => {
   reactiveCounterModel.value++
 })
+
+//chrome://flags/#enable-portals
+if ("HTMLPortalElement" in window) {
+  const style = document.createElement("style")
+  style.innerHTML = `
+  portal {
+    position:fixed;
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+    box-shadow: 0 0 20px 10px #999;
+    transform: scale(0.4);
+    transform-origin: bottom left;
+    bottom: 20px;
+    left: 20px;
+    animation-name: fade-in;
+    animation-duration: 1s;
+    animation-delay: 2s;
+    animation-fill-mode: forwards;
+  }
+  .portal-transition {
+    transition: transform 0.4s;
+  }
+  @media (prefers-reduced-motion: reduce) {
+    .portal-transition {
+      transition: transform 0.001s;
+    }
+  }
+  .portal-reveal {
+    transform: scale(1.0) translateX(-20px) translateY(20px);
+  }
+  @keyframes fade-in {
+    0%   { opacity: 0; }
+    100% { opacity: 1; }
+  }
+`
+
+  const portal = document.createElement("portal")
+  portal.src = "https://www.bbc.com"
+  portal.style = style
+  document.body.appendChild(portal)
+  // portal.activate()
+}
